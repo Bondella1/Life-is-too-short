0d56ce6300ce97c95b9feef38d8afe35eb292285
Bondella1 <chinazaemekaokoye051@gmail.com>
2025-02-08 13:05:11 -0500

    firebaseConfig

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBvwpelEmmLRnib9csA2uMmHVQMf4v0R3U",
+    authDomain: "lifes-too-short.firebaseapp.com",
+    projectId: "lifes-too-short",
+    storageBucket: "lifes-too-short.firebasestorage.app",
+    messagingSenderId: "235277476472",
+    appId: "1:235277476472:web:66f06e5724699fc685038d",
+    measurementId: "G-C83119GS9B"
+  };
---
+++ firebase/firebaseService.js
@@ -0,0 +1,2 @@
+import { collection, addDoc, getDocs, query, orderBy } from "firebase/firestore";
+import { db } from "./firebaseConfig";  // Import Firestore database instance
